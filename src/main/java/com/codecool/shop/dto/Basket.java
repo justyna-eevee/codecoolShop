09d551ca1a8049c6e05f139f81cfb851d13f414a
justyna-eevee <justyna.zaczyk@onet.pl>
2022-04-21 11:12:21 +0200

    create dto.Basket

---
+++ src/main/java/com/codecool/shop/dto/Basket.java
@@ -0,0 +1,38 @@
+package com.codecool.shop.dto;
+
+import com.codecool.shop.model.BasketProductModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basket {
+
+    protected int id;
+    private int userId;
+    private boolean payment;
+    List<Product> products = new ArrayList<>();
+
+    public Basket() {}
+
+    public Basket(int id, int userId, boolean payment) {
+        this.id = id;
+        this.userId = userId;
+        this.payment = payment;
+    }
+
+    public void addProductToBasket(Product product){
+        products.add(product);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public boolean isPayment() {
+        return payment;
+    }
+}
---
+++ src/main/java/com/codecool/shop/dto/Product.java
@@ -0,0 +1,4 @@
+package com.codecool.shop.dto;
+
+public class Product {
+}
