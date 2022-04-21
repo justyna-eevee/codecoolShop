eb1e4d52471da137e3f5c93c4d61c53145e97fcd
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2022-04-21 11:03:15 +0200

    Implemented Product DTO implemented

---
+++ src/main/java/com/codecool/shop/dto/Product.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.dto;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+
+    public Product(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public String getDescription() { return description; }
+
+    public void setId(int id) { this.id = id; }
+}
