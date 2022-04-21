1166cf29a2b4c92d64d2e43daaacda6f06f31c99
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2022-04-21 11:13:23 +0200

    Implemented Product DTO refactored

--- src/main/java/com/codecool/shop/dto/Product.java
+++ src/main/java/com/codecool/shop/dto/Product.java
@@ -4,11 +4,21 @@ public class Product {
     private int id;
     private String name;
     private String description;
+    private int price;
+    private String currency;
+    private int supplierId;
+    private int categoryId;
+    private String imagePath;
 
-    public Product(int id, String name, String description) {
+    public Product(int id, String name, String description, int price, String currency, int supplierId, int categoryId, String imagePath) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.price = price;
+        this.currency = currency;
+        this.supplierId = supplierId;
+        this.categoryId = categoryId;
+        this.imagePath = imagePath;
     }
 
     public int getId() { return id; }
@@ -16,6 +26,16 @@ public class Product {
     public String getName() { return name; }
 
     public String getDescription() { return description; }
+
+    public int getPrice() { return price; }
+
+    public String getCurrency() { return currency; }
+
+    public int getSupplierId() { return supplierId; }
+
+    public int getCategoryId() {  return categoryId; }
+
+    public String getImagePath() { return imagePath; }
 
     public void setId(int id) { this.id = id; }
 }
